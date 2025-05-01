52f9ec36e4491acc650eb6faadcb14021899acb5
devkbm <devkbm0417@gmail.com>
2025-05-01 21:25:55 +0900

    2025-05-01

---
+++ src/app/hrm/partner-staff/partner-staff-app.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partner-staff-app',
+  imports: [],
+  template: `<p>partner-staff-app works!</p>`,
+  styles: `
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PartnerStaffAppComponent { }
