7fd2e6393941e5d3d63132ab0eb6a896f47da837
devkbm <devkbm0417@gmail.com>
2025-07-07 22:13:01 +0900

    2025-07-07

--- src/app/core/global-property.ts
+++ src/app/core/global-property.ts
@@ -6,8 +6,9 @@ import { Injectable } from '@angular/core';
 export class GlobalProperty {
 
     //static serverUrl: string = "https://localhost:8090";          // 로컬 테스트용
-    static serverUrl: string = "https://158.180.86.87:8090";        // 오라클 클라우드
-    //static serverUrl: string = "https://152.69.237.87:443";
+    //static serverUrl: string = "https://158.180.86.87:8090";        // 오라클 클라우드
+    static serverUrl: string = "https://connect-one.zapto.org";
 
     //public static serverUrl: string = "http://kbm0417.gonetis.com:8090";
+
 }
