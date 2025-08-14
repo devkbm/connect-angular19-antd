ea21b6eeed45092d86cabe6fcabb50f0dfa8796a
devkbm <devkbm0417@gmail.com>
2025-08-14 18:20:01 +0900

    2025-08-14

--- src/app/third-party/ng-zorro/nz-page-header-custom/nz-page-header-custom.component.ts
+++ src/app/third-party/ng-zorro/nz-page-header-custom/nz-page-header-custom.component.ts
@@ -1,5 +1,5 @@
 import { Component, TemplateRef, inject, input } from '@angular/core';
-import { Location } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { MenuBreadCrumb, SessionManager } from 'src/app/core/session-manager';
@@ -11,6 +11,7 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 @Component({
   selector: 'nz-page-header-custom',
   imports: [
+    CommonModule,
     RouterModule,
     NzPageHeaderModule,
     NzBreadCrumbModule,
@@ -22,6 +23,8 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
       <nz-breadcrumb-item><a routerLink="/home"><span nz-icon [nzType]="'home'"></span></a></nz-breadcrumb-item>
     </nz-breadcrumb>
     -->
+
+    {{menuBreadCrumb|json}}
     <nz-page-header (nzBack)="goBack()" nzBackIcon [nzTitle]="title()" [nzSubtitle]="subtitle()">
       <nz-breadcrumb nz-page-header-breadcrumb nzSeparator=">" >
         <nz-breadcrumb-item><a routerLink="/home"><span nz-icon [nzType]="'home'"></span></a></nz-breadcrumb-item>
